8594d486e5c1874fb5d8351a3e518f60c09652ac
ankit-kumar-pathak <ak8672053@gmail.com>
2020-11-21 03:00:49 +0530

    Modified main.js in require folder

--- Lecture11/require/main.js
+++ Lecture11/require/main.js
@@ -8,6 +8,20 @@ if (typeof window !== 'undefined') {
     console.log('running in node')
 }
 
+/*
+# global,module and process these are three objects which exist in node but does not
+exist in browser.
+document and window exist in browser and does not exist in node.
+
+# globalThis exist in both nodejs and browser.
+In browser globalThis === window gives true.
+In nodejs globalThis === global gives true.
+globalThis is basically common way to access outermost scope.
+
+# Javascript me hum jo cheeze export krte h usko bhi runtime pe badalna possible h
+after it has been exported.
+*/
+
 console.log(lib1)
 
 lib1.sayHello('John')
@@ -15,3 +29,4 @@ lib1.sayHello('John')
 setTimeout(() => {
     console.log(lib1)
 }, 2000)
+
