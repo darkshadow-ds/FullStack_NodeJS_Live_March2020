d1a133eb4d69b705ac4bbb4c4fbe369ad6c5f630
ankit-kumar-pathak <ak8672053@gmail.com>
2020-11-22 21:19:43 +0530

    set input elements

--- Lecture11/express-intro/server.js
+++ Lecture11/express-intro/server.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
 function middleware1 (req, res, next) {
     console.log(req.url)
     res.send('sidubgisdugb')
-    next()
+    next() // Here next() call middleware2 according to app.get('/x', middleware1, middleware2).
 }
 function middleware2 (req, res, next) {
     console.log(req.hostname)
@@ -19,9 +19,30 @@ function middleware2 (req, res, next) {
 
 
 app.get('/x', middleware1, middleware2)
+/*
+ Here after getting get request at '/x' middleware1 will run and due to next() 
+in middleware1, middleware2 will run.
+
+*/
 
 
 
 app.listen(3333, () => {
     console.log('server started on http://0.0.0.0:3333')
 })
+/*
+node_modules is folder ko delete krne ke baad jb hum npm install likhte h terminal me jo package.json
+waali saari dependencies install ho jaati h. General practice ye hoti h ki jb hum code share kre to 
+node_modules wale package ko delete krke code share kre.
+
+Use port number from 1025 to 65536.
+https server use 443 port number.
+http server use 80 port number
+
+GET request ke ander sirf url hoti h.
+
+Ek request ke corresponding ek hi request ho sakta h.
+
+Function jo request aane pr call ho jayenge apne aap hum unhe aur unke ander hum response ko send krne
+ka kaam krenge aise function ko hum middleware function ya phir handler function bolte h.
+*/
